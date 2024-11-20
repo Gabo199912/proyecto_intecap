e2bd2d5186c93cee9d634dac3fa9568639666877
Angel Vasquez <angelvasquez199912@icloud.com>
2024-11-19 20:58:36 -0600

    nevas funcionalidades y pagina web al 70%

--- codigo.js
+++ codigo.js
@@ -18,3 +18,20 @@ function removerClase(){
 };
 
 
+const mapa = document.getElementById('titulo-mapa');
+let texto = "Muestra tu Ubicación"
+
+
+
+
+function mostrarTitulo(objeto, texto, indice = 0){
+        
+        objeto.textContent += texto[indice]
+
+        
+        if(indice === texto.length-1) return;
+        setTimeout(() => mostrarTitulo(mapa, texto, indice+1), 150 )
+        
+}
+
+mostrarTitulo(mapa, texto);
